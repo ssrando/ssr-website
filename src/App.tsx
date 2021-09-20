165df58e878e99932baddd172266be229aacda73
cjs8487 <cjschaefer1999@gmail.com>
2021-09-19 22:23:03 -0400

    added current site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Nightly Download Links</h1>
+        <a href="https://nightly.link/ssrando/ssrando/workflows/build.yaml/master">Master (mostly stable)</a>
+        <br />
+        <a href="https://nightly.link/ssrando/ssrando/workflows/build.yaml/gui-redesign">Dev</a>
+        <br />
+        <a href="https://nightly.link/battlecats59/sslib/workflows/build.yaml/gui-redesign">Closed AC and FS (Battlecats Dev Fork)</a>
+        <br />
+        <a href="https://nightly.link/battlecats59/sslib/workflows/build.yaml/temp-music-rando">Music Rando (Battlecats Dev Fork)</a>
+
+        <h1>Asyncs</h1>
+        <h2>June 2021</h2>
+        <a href="https://nightly.link/ssrando/ssrando/actions/runs/896836207">Randomizer Build</a>
+        <br />
+        <a href="http://bombch.us/DQt-">Async Sheet</a>
+        <h2>July 2021</h2>
+        <a href="https://nightly.link/ssrando/ssrando/actions/runs/981921528">Randomizer Build</a>
+        <br />
+        <a href="http://bombch.us/DQ8J">Async Sheet</a>
+        <h2>August 2021</h2>
+        <a href="https://nightly.link/lepelog/sslib/actions/runs/1086121520">Randomizer Build</a>
+        <br />
+        <a href="http://bombch.us/DRF-">Async Sheet</a>
+        <h2>September 2021</h2>
+        <a href="#">Randomizer Build (TBA)</a>
+        <br />
+        <a href="http://bombch.us/DROk">Async Sheet</a>
     </div>
   );
 }
