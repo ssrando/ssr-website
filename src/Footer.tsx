9617e4b0087d72afcdc1aae629d70bc9f366befd
cjs8487 <cjschaefer1999@gmail.com>
2023-02-02 20:37:46 -0600

    fix footer covering other components

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,18 +5,21 @@ import { AppBar, Box, Toolbar } from "@mui/material";
 class Footer extends React.Component {
   render() {
     return (
-        <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
-            <Toolbar sx={{flexDirection: 'column'}}>
-                <Box sx={{ flexGrow: 1 }}/>
-                <Typography variant="h6">
-                    Skyward Sword Randomizer
-                </Typography>
-                <Typography variant="body2">
-                    In-game imagery and system logos Copyright © Nintendo, 2011-2021 - The Skyward Sword Randomizer community does <b>not</b> share copyrighted material
-                </Typography>
-                <Box sx={{ flexGrow: 1 }}/>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+                <Toolbar sx={{flexDirection: 'column'}}>
+                    <Box sx={{ flexGrow: 1 }}/>
+                    <Typography variant="h6">
+                        Skyward Sword Randomizer
+                    </Typography>
+                    <Typography variant="body2">
+                        In-game imagery and system logos Copyright © Nintendo, 2011-2023 - The Skyward Sword Randomizer community does <b>not</b> share copyrighted material
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }}/>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+        </>
     );
   }
 }
