3484338ae49d8c224fa4baa18ebe89ed8cb3718b
cjs8487 <cjschaefer1999@gmail.com>
2023-08-20 22:13:05 -0500

    get hook translates 404s to no data instead of error

--- src/controller/Hooks.ts
+++ src/controller/Hooks.ts
@@ -1,7 +1,16 @@
 import useSWR from 'swr';
 
 export const useGetApi = <T>(route: string) =>
-    useSWR<T>(route, (path) => fetch(path).then((res) => res.json()));
+    useSWR<T>(route, (path) =>
+        fetch(path).then((res) => {
+            if (!res.ok) {
+                if (res.status === 404) {
+                    return undefined;
+                }
+            }
+            return res.json();
+        }),
+    );
 
 export const useFileGet = (route: string) =>
     useSWR<string>(route, (path) => fetch(path).then((res) => res.text()));
